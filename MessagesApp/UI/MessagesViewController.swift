29134c302c8f4ec93261298856452ae4a5333430
Dariusz Rybicki <darrarski@gmail.com>
2017-04-19 23:55:12 +0200

    Add init to MessagesViewController

--- MessagesApp/UI/MessagesViewController.swift
+++ MessagesApp/UI/MessagesViewController.swift
@@ -2,6 +2,14 @@ import UIKit
 
 class MessagesViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func loadView() {
         view = UIView(frame: .zero)
         view.backgroundColor = .white
