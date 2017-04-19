42d3d7b0645d66b6d434935affd2fee575fe6866
Dariusz Rybicki <darrarski@gmail.com>
2017-04-20 00:11:47 +0200

    Add UICollectionView subview to MessagesView

--- MessagesApp/UI/MessagesView.swift
+++ MessagesApp/UI/MessagesView.swift
@@ -1,14 +1,46 @@
 import UIKit
+import SnapKit
 
 class MessagesView: UIView {
 
     init() {
         super.init(frame: .zero)
         backgroundColor = .white
+        loadSubviews()
+        setupLayout()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Subviews
+
+    let collectionView: UICollectionView = Factory.collectionView
+
+    private func loadSubviews() {
+        addSubview(collectionView)
+    }
+
+    // MARK: Layout
+
+    private func setupLayout() {
+        collectionView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+
+}
+
+private extension MessagesView {
+
+    struct Factory {
+
+        static var collectionView: UICollectionView {
+            let layout = UICollectionViewFlowLayout()
+            let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+            view.backgroundColor = .clear
+            return view
+        }
+
     }
 
 }
