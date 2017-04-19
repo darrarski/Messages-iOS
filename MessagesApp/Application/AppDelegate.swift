fcbbdd13f55f96b8d0cf71e5591a1c5c6a6b4746
Dariusz Rybicki <darrarski@gmail.com>
2017-04-19 22:36:09 +0200

    Create iOS app project

---
+++ MessagesApp/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
---
+++ MessagesApp/UI/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        view = UIView(frame: .zero)
+        view.backgroundColor = .white
+    }
+
+}
