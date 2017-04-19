86f54674c0a1713a28168ffd9c670c99526c6e43
Dariusz Rybicki <darrarski@gmail.com>
2017-04-20 00:20:53 +0200

    Bootstrap collection view setup

--- MessagesApp/UI/MessagesViewController.swift
+++ MessagesApp/UI/MessagesViewController.swift
@@ -20,4 +20,53 @@ class MessagesViewController: UIViewController {
         view = MessagesView()
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+
+    // MARK: CollectionView
+
+    let collectionViewLayout = UICollectionViewFlowLayout()
+
+    private func setupCollectionView() {
+        messagesView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        messagesView.collectionView.dataSource = self
+        messagesView.collectionView.delegate = self
+        messagesView.collectionView.collectionViewLayout = collectionViewLayout
+    }
+
+}
+
+extension MessagesViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        guard section == 0 else { return 0 }
+        return 100
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .lightGray
+        return cell
+    }
+
+}
+
+extension MessagesViewController: UICollectionViewDelegate {}
+
+extension MessagesViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+
 }
