da28e0ce9d82d39c0bd221b218f19eba126b88a1
Dariusz Rybicki <darrarski@gmail.com>
2017-04-25 00:58:22 +0200

    Add OutgoingMessageSectionController

---
+++ MessagesApp/UI/Controllers/OutgoingMessageSectionController.swift
@@ -0,0 +1,62 @@
+import IGListKit
+
+class OutgoingMessageSectionController: IGListSectionController {
+
+    override init() {
+        super.init()
+        inset = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10)
+    }
+
+    fileprivate(set) var viewModel: OutgoingMessageViewModel?
+
+    fileprivate let cellPrototype: MessageCell = {
+        let cell = MessageCell(frame: .zero)
+        cell.prepareForComputingHeight()
+        return cell
+    }()
+
+    // MARK: Cell configuration
+
+    fileprivate func configureCell(_ cell: MessageCell) {
+        cell.label.text = viewModel?.text
+        cell.bubblePosition = .right
+        cell.bubbleView.backgroundColor = UIColor(red:0.01, green:0.48, blue:0.98, alpha:0.8)
+        cell.label.textColor = .white
+        cell.label.textAlignment = .left
+        cell.activityIndicator.startAnimating()
+    }
+
+}
+
+extension OutgoingMessageSectionController: IGListSectionType {
+
+    func numberOfItems() -> Int {
+        return 1
+    }
+
+    func sizeForItem(at index: Int) -> CGSize {
+        guard let context = collectionContext else { fatalError() }
+        let width = context.containerSize.width - inset.left - inset.right
+        cellPrototype.prepareForReuse()
+        configureCell(cellPrototype)
+        let height = cellPrototype.computeHeight(forWidth: width)
+        return CGSize(width: width, height: height)
+    }
+
+    func cellForItem(at index: Int) -> UICollectionViewCell {
+        guard let cell = collectionContext?.dequeueReusableCell(
+            of: MessageCell.self,
+            for: self,
+            at: index) as? MessageCell else { fatalError() }
+        configureCell(cell)
+        return cell
+    }
+
+    func didUpdate(to object: Any) {
+        guard let viewModel = object as? OutgoingMessageViewModel else { fatalError() }
+        self.viewModel = viewModel
+    }
+
+    func didSelectItem(at index: Int) {}
+
+}
