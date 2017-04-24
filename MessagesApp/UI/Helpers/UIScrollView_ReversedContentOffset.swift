3ae70385a42753f8889f85c3eac07ee4a42a1b43
Dariusz Rybicki <darrarski@gmail.com>
2017-04-25 00:54:47 +0200

    Update UIScrollView_ReversedContentOffset extension
    - add setReversedContentOffset(_: CGPoint, animated: Bool) function
    - refactor

--- MessagesApp/UI/Helpers/UIScrollView_ReversedContentOffset.swift
+++ MessagesApp/UI/Helpers/UIScrollView_ReversedContentOffset.swift
@@ -3,18 +3,19 @@ import UIKit
 extension UIScrollView {
 
     var reversedContentOffset: CGPoint {
-        get {
-            return CGPoint(
-                x: contentSize.width - contentOffset.x - min(visibleSize.width, contentSize.width),
-                y: contentSize.height - contentOffset.y - min(visibleSize.height, contentSize.height)
-            )
-        }
-        set {
-            contentOffset = CGPoint(
-                x: contentSize.width - newValue.x - min(visibleSize.width, contentSize.width),
-                y: contentSize.height - newValue.y - min(visibleSize.height, contentSize.height)
-            )
-        }
+        get { return reversedContentOffset(for: contentOffset) }
+        set { contentOffset = reversedContentOffset(for: newValue) }
+    }
+
+    func setReversedContentOffset(_ reversedContentOffset: CGPoint, animated: Bool) {
+        setContentOffset(self.reversedContentOffset(for: reversedContentOffset), animated: animated)
+    }
+
+    private func reversedContentOffset(for contentOffset: CGPoint) -> CGPoint {
+        return CGPoint(
+            x: contentSize.width - contentOffset.x - min(visibleSize.width, contentSize.width),
+            y: contentSize.height - contentOffset.y - min(visibleSize.height, contentSize.height)
+        )
     }
 
     private var visibleSize: CGSize {
